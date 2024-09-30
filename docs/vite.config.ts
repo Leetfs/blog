9581ed72eb86a10297504806127b8a58b4f85610
Lee <lee@mtftm.com>
2024-10-01 02:06:11 +0800

    Update build command in cf.yml

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://leetfs.com',
-        // uncomment the following line to use local server
-        // target: 'http://localhost:8787',
-        changeOrigin: true,
-        autoRewrite: true,
-      },
-    },
-  },
   ssr: { noExternal: true },
 })
